814095f78aa60eda63c8c074082a1fa0d81b2b7f
Ahmad Fadillah <13020180149@umi.ac.id>
2023-06-02 14:07:20 +0800

    update

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -109,12 +109,92 @@
                         </div>
                     </div>
                 </div>
+
             </div>
+
             <img src="{{ asset('iteck') }}/assets/img/header/hand_megaphone.png" alt="" class="hand-mega slide_up_down">
             <img src="{{ asset('iteck') }}/assets/img/header/head6_rating.png" alt="" class="head6-rating scale_up_down">
             <img src="{{ asset('iteck') }}/assets/img/header/header5_linechart.png" alt="" class="head6-charts scale_up_down">
             <img src="{{ asset('iteck') }}/assets/img/header/rocket.png" alt="" class="head6-rocket">
+
         </header>
+        <header class="style-4">
+            <div class="content">
+                <div class="container">
+                    <div class="row align-items-center justify-content-between">
+                        <div class="col-lg-5 order-2 order-lg-0">
+                            <div class="info">
+                                <div class="section-head style-4">
+                                    <small class="title_small">Tentang </small>
+                                    <h2 class="mb-30">Informasi Tentang <span> Sekolah </span> </h2>
+                                </div>
+                                <div class="faq style-3 style-4">
+                                    <div class="accordion" id="accordionExample">
+                                        <div class="accordion-item">
+                                            <h2 class="accordion-header" id="heading1">
+                                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse1" aria-expanded="true" aria-controls="collapse1">
+                                                    Data Sekolah
+                                                </button>
+                                            </h2>
+                                            <div id="collapse1" class="accordion-collapse collapse show" aria-labelledby="heading1" data-bs-parent="#accordionExample">
+                                                <div class="accordion-body">
+                                                    (40305166) SMAN 8 PINRANG <a href="https://sekolah.data.kemdikbud.go.id/index.php/Chome/profil/37A3756E-293F-497D-9B79-8C9CA0F38CD4" style="color:aqua">Selengkapnya</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="accordion-item">
+                                            <h2 class="accordion-header" id="heading2">
+                                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse2" aria-expanded="false" aria-controls="collapse2">
+                                                    Riwayat Akreditasi
+                                                </button>
+                                            </h2>
+                                            <div id="collapse2" class="accordion-collapse collapse show" aria-labelledby="heading2" data-bs-parent="#accordionExample">
+                                                <div class="accordion-body">
+                                                    Tahun : 2017 <br>
+                                                    No SK : 160/SK/BAP-SM/XI/2017<br>
+                                                    Tanggal SK : 23-Nov-2017<br>
+                                                    Tanggal Akhir SK : -<br>
+                                                    Standar Isi : 89<br>
+                                                    Standar Proses : 88<br>
+                                                    Standar Kompetensi Lulusan : 89<br>
+                                                    Standar PTK : 84<br>
+                                                    Standar Sarpras : 85<br>
+                                                    Standar Pengelolaan : 93<br>
+                                                    Standar Pembiayaan : 97<br>
+                                                    Standar Penilaian : 88<br>
+                                                    Nilai Akhir : 89<br>
+                                                    Peringkat : B
+                                                </div>
+                                            </div>
+                                        </div>
+                                        {{-- <div class="accordion-item">
+                                            <h2 class="accordion-header" id="heading3">
+                                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse3" aria-expanded="false" aria-controls="collapse3">
+                                                    Protect your note with lock
+                                                </button>
+                                            </h2>
+                                            <div id="collapse3" class="accordion-collapse collapse show" aria-labelledby="heading3" data-bs-parent="#accordionExample">
+                                                <div class="accordion-body">
+                                                    Images, videos, PDFs and audio files are supported. Create math expressions and diagrams directly from the app. Take photos with the mobile app and save them to a note.
+                                                </div>
+                                            </div>
+                                        </div> --}}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 order-0 order-lg-2">
+                            <div class="img mb-30 mb-lg-0">
+                                <img src="{{ asset('iteck/assets/img/sekolah.jpg') }}" alt="" style="border-radius:2%">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <img src="assets/img/header/header_4_bubble.png" alt="" class="bubble">
+            </div>
+            <img src="assets/img/header/header_4_wave.png" alt="" class="wave">
+        </header>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3982.177554266432!2d119.52793897368649!3d-3.54649094227116!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d945df47a6376fd%3A0x6d486d7b85b77ee!2sSMA%20Negeri%208%20Pinrang!5e0!3m2!1sen!2sid!4v1685685534456!5m2!1sen!2sid" width="1270" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         <!-- ====== end about ====== -->
     </main>
     <!--End-Contents-->
